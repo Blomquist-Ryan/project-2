f6e733292271e128ca0d48bbba1ddd62589a04d1
Ryan Blomquist <rblomquist96@gmail.com>
2019-06-25 20:27:10 -0600

    init commit

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var app = express();
+app.use(express.static('/public'));
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/home', (req, res) => res.render('pages/home'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
